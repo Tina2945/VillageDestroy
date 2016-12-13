f827177d9ad81828487855028936d1025b0f7162
tina2945 <tina2945@gmail.com>
2016-12-13 18:57:28 +0800

    hi

---
+++ assets/JumpSenser.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpSenser : MonoBehaviour {
+
+    private int TouchedCount = 0;
+    void OnTriggerEnter(Collider other)
+    {
+        TouchedCount++;
+    }
+    void OnTriggerExit(Collider other)
+    {
+        TouchedCount--;
+    }
+    public bool IsCanJump()
+    {
+        return TouchedCount > 0;
+    }
+}
